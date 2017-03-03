cd19bb68f558f7c7298b1670cf9bf394cc44d706
Matt Sams <matts@customwareinc.com>
2017-03-03 09:48:50 -0500

    Clean up of merge mess.

--- src/main/kotlin/com/thedeadpixelsociety/twodee/systems/DollySystem.kt
+++ src/main/kotlin/com/thedeadpixelsociety/twodee/systems/DollySystem.kt
@@ -7,10 +7,7 @@ import com.badlogic.ashley.systems.IteratingSystem
 import com.badlogic.gdx.utils.Pools
 import com.thedeadpixelsociety.twodee.components.Dolly
 import com.thedeadpixelsociety.twodee.components.Transform
-<<<<<<< HEAD
 import com.thedeadpixelsociety.twodee.reset
-=======
->>>>>>> 47cfe804620e5ba7078eb0724ee191c01426a486
 
 /**
  * A dolly system used to run transformation actions against a camera.
@@ -26,16 +23,10 @@ class DollySystem : IteratingSystem(Family.all(Transform::class.java, Dolly::cla
 
         runActions(deltaTime, dolly, transform)
 
-<<<<<<< HEAD
         dolly.camera.reset(false)
         dolly.camera.position.set(-(dolly.camera.viewportWidth * .5f), -(dolly.camera.viewportHeight * .5f), 0f)
         dolly.camera.rotate(transform.angle)
         dolly.camera.position.set(transform.position, 0f)
-=======
-        dolly.camera.translate(-transform.position.x, -transform.position.y)
-        dolly.camera.rotate(transform.deltaAngle)
-        dolly.camera.translate(transform.position.x, transform.position.y)
->>>>>>> 47cfe804620e5ba7078eb0724ee191c01426a486
         dolly.camera.update()
     }
 
